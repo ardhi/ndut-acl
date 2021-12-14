3827a2f9ec1968ac6970ac80b423ce3c5f1b4dbe
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-14 08:50:57 +0700

    Route protect

--- ndutRest/hook/on-request.js
+++ ndutRest/hook/on-request.js
@@ -1,9 +1,13 @@
 module.exports = async function (request, reply) {
   if (!request.user) return
+  if (!request.protectedRoute) return
+  const { _, outmatch } = this.ndut.helper
   const roleUser = await this.ndutDb.findOne('AclRoleUser', request, { where: { userId: request.user.id } })
   if (!roleUser) throw this.Boom.forbidden('User doesn\'t have any role yet')
   const role = await this.ndutDb.findOne('AclRole', request, { where: { id: roleUser.roleId, status: 'ENABLED' } })
   if (!role) throw this.Boom.forbidden('No such role found or role is currently disabled')
-  const roleRoute = await this.ndutDb.findOne('AclRoleRoute', request, { where: { roleId: roleUser.roleId } })
-  console.log(roleRoute)
+  const roleRoutes = await this.ndutDb.find('AclRoleRoute', request, { where: { roleId: roleUser.roleId } })
+  const isMatch = this.ndutAuth.helper.routeMatch(request, roleRoutes)
+  if (!isMatch) throw this.Boom.forbidden('Access Denied')
+  request.role = role
 }
