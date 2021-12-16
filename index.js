8ee53f74a37290d89f7fea764ec18add7cc18a82
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-16 19:10:43 +0700

    Refactor & version bump

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = async function (fastify) {
+module.exports = async function () {
   const name = 'ndut-acl'
   const dependency = ['ndut-db', 'ndut-auth']
   return { name, dependency }
